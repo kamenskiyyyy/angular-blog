afa2d3a9d0df9e4ff7f12d7e3f0d3b11c13809f6
Kamenev Nikolay <79969997909@mail.ru>
2021-07-19 11:03:39 +0300

    creating a service for authorization

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.service";
+import {Router} from "@angular/router";
+import {User} from "../../shared/interfaces";
 
 @Component({
   selector: 'app-login-page',
@@ -10,7 +13,10 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup
 
-  constructor() {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
   }
 
   ngOnInit(): void {
@@ -30,5 +36,15 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return
     }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset()
+      this.router.navigate(['/admin', 'dashboard'])
+    })
   }
 }
