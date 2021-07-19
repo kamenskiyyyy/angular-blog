56f3f2a7ea8f19ff146cd174e18f3fdf0cce9726
Kamenev Nikolay <79969997909@mail.ru>
2021-07-19 13:11:22 +0300

    firebase setup

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../shared/services/auth.service";
 import {Router} from "@angular/router";
-import {User} from "../../shared/interfaces";
+import {User} from "../../shared/fbAuthResponce";
 
 @Component({
   selector: 'app-login-page',
